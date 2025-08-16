d1d61d84bcdeae7f71caec80c75d71333313387b
Otis-Ruby <ujval25072004@gmail.com>
2025-08-16 10:04:58 +0530

    Update candidateRoutes.js Added Routed for get records

--- candidate_check/routes/candidateRoutes.js
+++ candidate_check/routes/candidateRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {login,signup,updateProfile,getProfile,getAllHiring,getRoundsByHiringId,applyToHiring} = require('../controllers/candidateController');
+const {login,signup,updateProfile,getProfile,getAllHiring,getRoundsByHiringId,applyToHiring,getCandidateRecords} = require('../controllers/candidateController');
 const { authorizeMiddleware } = require('../middleware/authorizeMiddleware');
 
 // Public Routes
@@ -14,6 +14,8 @@ router.put('/updateprofile', authorizeMiddleware("candidate"), updateProfile);
 router.get('/allhiring', authorizeMiddleware("candidate"), getAllHiring);
 router.get('/rounds/:hiringId', authorizeMiddleware("candidate"), getRoundsByHiringId);
 router.post('/apply/:hiringId', authorizeMiddleware("candidate"), applyToHiring);
+router.get('/getrecords', authorizeMiddleware("candidate"), getCandidateRecords);
 
 
 module.exports = router;
+
