2362d30dd3f6e1ddf67906a0965be80f13bd8280
Otis-Ruby <ujval25072004@gmail.com>
2025-08-16 19:08:05 +0530

    Update server.js

--- candidate_check/server.js
+++ candidate_check/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const candidateRoutes = require('./routes/candidateRoutes');
+const validateRoutes = require('./routes/validateRoutes');
 
 dotenv.config();
 
@@ -9,6 +10,7 @@ const app = express();
 app.use(express.json());
 
 app.use('/api/candidate', candidateRoutes);
+app.use('/api/validate',validateRoutes);
 
 // Connect to MongoDB and start server
 mongoose.connect(process.env.MONGO_URI)
@@ -18,3 +20,4 @@ mongoose.connect(process.env.MONGO_URI)
         });
     })
     .catch((err) => console.error("MongoDB connection failed:", err));
+
